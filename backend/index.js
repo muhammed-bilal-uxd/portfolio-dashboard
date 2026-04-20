508876c1d641b07c0d3d7ddc2707f3eeda52bd0b
bilal <mail2muhammedbilal@gmail.com>
2026-04-21 00:10:30 +0530

    fix: vicky pc can't mongo not working

--- backend/index.js
+++ backend/index.js
@@ -45,13 +45,14 @@ const allowedOrigins = process.env.FRONTEND_URL
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
+    // origin: function (origin, callback) {
+    //   if (!origin || allowedOrigins.includes(origin)) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error("Not allowed by CORS"));
+    //   }
+    // },
     credentials: true,
   })
 );
